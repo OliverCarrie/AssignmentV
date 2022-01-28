019309dc28eb1c58281ba68410c2482b3bed6af4
Oliver Carrie <oliver.carrie@student.uni-tuebingen.de>
2022-01-28 11:29:59 +0100

    Perform GET command and store it in a dataframe

--- AssignmentV.R
+++ AssignmentV.R
@@ -2,6 +2,7 @@
 ########## Assignment V ###############################
 #######################################################
 
+# Rate limit is 5000 per day and 5 per second, so we will need to put in a pause-timer
 
 ########################################
 # 3. Interact with the API - Basics
@@ -14,3 +15,6 @@ library(httr)
 # Perform a first GET request, that searches for event venues in Germany (countryCode = "DE"). Extract
 # the content from the response object and inspect the resulting list. Describe what you can see.
 
+event_location <- GET(url = "https://app.ticketmaster.com/discovery/v2/venues?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&countryCode=DE")
+
+event_DE <- jsonlite::fromJSON(content(event_location, as = "text"))
