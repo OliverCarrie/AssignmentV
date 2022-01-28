1e043898408f7a46e02b7d30e7018761a0c6a387
Oliver Carrie <oliver.carrie@student.uni-tuebingen.de>
2022-01-28 11:07:47 +0100

    Begin of Exercise 3, library and GET request

---
+++ AssignmentV.R
@@ -0,0 +1,16 @@
+#######################################################
+########## Assignment V ###############################
+#######################################################
+
+
+########################################
+# 3. Interact with the API - Basics
+########################################
+
+# Load the packages needed to interact with APIs using R
+
+library(httr)
+
+# Perform a first GET request, that searches for event venues in Germany (countryCode = "DE"). Extract
+# the content from the response object and inspect the resulting list. Describe what you can see.
+
